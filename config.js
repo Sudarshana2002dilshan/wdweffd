b6c91c075534cb764c54c7d327bb73a5ddabbe8b
Dilshan <134767248+Sudarshana2002dilshan@users.noreply.github.com>
2025-06-12 20:02:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['62']
+global.packname = "Sticker By Denny"
+global.author = "DennyOfficial"
+global.mess = {
+ owner: '*You are not the owner*',
+ premium: '*You are not premium*'
+}
